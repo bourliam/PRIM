5698333c91f54e4165cab705b1061ffdaa874731
Mohammed Lamine Bouchouia <ml.bouchouia@outlook.com>
2018-12-14 17:38:56 +0100

    segments length and max speed + points and cars counts

--- source/OsmProcessing.py
+++ source/OsmProcessing.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import time 
+from DataProcessing import reverseVincenty
 def getSegments(    osmWays,innerBox=[[[-1.5460, 48.1656], [-1.5460, 48.0632], [-1.7626, 48.0632], [-1.7626,48.1656], [-1.5460, 48.1656]]],
                     innerTags =["motorway", "trunk", "primary", "secondary", "tertiary", "motorway_link", "trunk_link", "primary_link", "secondary_link", "tertiary_link"],
                     outerBox=[[[-1.4460, 48.2056], [-1.4460, 48.0032], [-1.8626, 48.0032], [-1.8626,48.2056], [-1.4460, 48.2056]]],
@@ -44,6 +45,7 @@ def getSegments(    osmWays,innerBox=[[[-1.5460, 48.1656], [-1.5460, 48.0632], [
     segments=pd.concat([innerRocadeDF,outerRocadeDF])
     segments.drop_duplicates('_id',inplace=True)
     segments.reset_index(drop=True,inplace=True)
+    segments.tag=segments.tag.apply(lambda x : dict([(v['k'],v['v']) for v in x]))
     return segments.set_index('_id')
 
 
@@ -83,11 +85,18 @@ def parallelClosestSegment(myPoint):
     ).limit(1)
     return next(res,{'_id':'N/A'})['_id']
 
-def buildSegmentHierarchy(segments):
+def buildSegmentsMeta(segments, points):
     """ 
     find all incoming/outgoins segments for each segment
+    compute the legth of each segment
+    assign Max speed for each segment
     segments: pandas dataframe of segmenst (OSM way)
     """
-    ins =segments.nodes.apply(lambda x : segments.index[ segments.nodes.apply(lambda y : ((y[len(y)-1] in x) or (x[0] in y)) and x!=y )].values)
-    outs=segments.nodes.apply(lambda x : segments.index[ segments.nodes.apply(lambda y : ((x[len(x)-1] in y) or (y[0] in x)) and x!=y )].values)
-    return segments.assign(ins=ins,outs=outs)
+    segments=segments.assign(maxSpeed=segments.tag.apply(lambda x : x['maxspeed'] if 'maxspeed'in x.keys() else '' ))
+    distance = segments['loc'].apply(lambda x : sum([reverseVincenty(a,b) for a, b in zip(x['coordinates'][:-1],x['coordinates'][1:])]))
+    ins =segments.nodes.apply(lambda x : segments.index[segments.nodes.apply(lambda y : ((y[len(y)-1] in x) or (x[0] in y)) and x!=y )].values)
+    outs=segments.nodes.apply(lambda x : segments.index[segments.nodes.apply(lambda y : ((x[len(x)-1] in y) or (y[0] in x)) and x!=y )].values)
+    pointCounts=points.groupby(['matching_road']).size()
+    carCounts = points.groupby(['matching_road','id']).size().groupby(['matching_road']).size()
+    return segments.assign(ins=ins, outs=outs, distance = distance,pointCounts=pointCounts,carCounts=carCounts)
+
