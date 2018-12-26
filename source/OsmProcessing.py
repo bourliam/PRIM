854a3198d728eca14fbd50072eafc7e3cd30505b
Mohammed Lamine Bouchouia <ml.bouchouia@outlook.com>
2018-12-26 17:12:09 +0100

    -- revised roads incoming/outgoing flow -- function to extract oneway tag

--- source/OsmProcessing.py
+++ source/OsmProcessing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import time 
 from DataProcessing import reverseVincenty
 def getSegments(    osmWays,innerBox=[[[-1.5460, 48.1656], [-1.5460, 48.0632], [-1.7626, 48.0632], [-1.7626,48.1656], [-1.5460, 48.1656]]],
@@ -107,11 +108,30 @@ def buildSegmentsMeta(segments, points):
     points : pandas dataframe
         logs
     """
+    noOneWays=segments[segments.oneWay!=1].copy()
+    noOneWays.nodes=noOneWays.nodes.apply(lambda x : x[::-1])
+    segs=pd.concat([segments,noOneWays])
+    ins = segs.nodes.apply(lambda x : segs.index[segs.nodes.apply(lambda y : ((y[len(y)-1] in x[:-1]) or (x[0] in y[1:])) and x!=y and x!=y[::-1])].values)
+    outs = segs.nodes.apply(lambda x : segs.index[segs.nodes.apply(lambda y : ((x[len(x)-1] in y[:-1]) or (y[0] in x[1:])) and x!=y and x!=y[::-1])].values)
+    ins = ins.groupby('_id').apply(lambda x : np.unique(np.concatenate([*x])))
+    outs = outs.groupby('_id').apply(lambda x : np.unique(np.concatenate([*x])))
+    
     segments=segments.assign(maxSpeed=segments.tag.apply(lambda x : x['maxspeed'] if 'maxspeed'in x.keys() else '' ))
     length = segments['loc'].apply(lambda x : sum([reverseVincenty(a,b) for a, b in zip(x['coordinates'][:-1],x['coordinates'][1:])]))
-    ins =segments.nodes.apply(lambda x : segments.index[segments.nodes.apply(lambda y : ((y[len(y)-1] in x) or (x[0] in y)) and x!=y )].values)
-    outs=segments.nodes.apply(lambda x : segments.index[segments.nodes.apply(lambda y : ((x[len(x)-1] in y) or (y[0] in x)) and x!=y )].values)
     pointCounts=points.groupby(['matching_road']).size()
     carCounts = points.groupby(['matching_road','id']).size().groupby(['matching_road']).size()
     return segments.assign(ins=ins, outs=outs, length = length,pointCounts=pointCounts,carCounts=carCounts)
 
+def setOneWay(segments):
+    """
+    extract one way tag from Osm data
+    
+    segments: pandas dataframe
+        Osm ways
+    """
+    return segments.assign(oneWay=segments.tag.apply(lambda x :
+                                                                  1 if ('oneway' in x and (x['oneway']=='yes')) 
+                                                                      or(x['highway']=='motorway') 
+                                                                      or('junction' in x and(x['junction'] in ['circular','roundabout'] ))
+                                                            else -1 if  ('oneway' in x and x['oneway']=='-1' ) 
+                                                            else  0))
